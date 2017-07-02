79ef2b2376a04a0e3cfb60cc9eeaa1f85c4160ed
Wang <gavin19950511@gmail.com>
2017-07-02 15:44:45 +0800

    Update PostDemo.py

--- PostDemo.py
+++ PostDemo.py
@@ -1,7 +1,12 @@
 import requests as rq
 
+#add Device
 r = rq.post("http://140.130.36.51/HelloGPIO/index.php",data={"Act":"addDevice","Pin_id":"14","Cost":"100","Name":"light"})
+
+#add User
 #r = rq.post("http://140.130.36.51/HelloGPIO/index.php",data={"Act":"addUser","User_id":"4024xxxx","Mail":"4024xxxx@gm.nfu.edu.tw","Name":"P.K"})
+
+#add command
 #r = rq.post("http://140.130.36.51/HelloGPIO/index.php",data={"Act":"CMD","Pin_id":"14","User_id":"4024xxxx","CMD":"on"})
 print r.status_code
 print r.text
